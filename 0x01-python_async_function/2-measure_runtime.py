200ce2121382a96618d2134250c67b945b380044
CHIGODI <chiegody254@gmail.com>
2024-07-08 17:55:43 +0300

    Fix pycodestyle

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,9 +1,15 @@
+"""
+    Measure the total execution time for wait_n(n, max_delay),
+    and return total_time / n.
+"""
+
 import asyncio
 import time
 from typing import List
 
 # Import wait_n function from the 1-concurrent_coroutines module
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,5 +1,9 @@
-wait_random = __import__('0-basic_async_syntax').wait_random
+"""
+    Regular function that creates and returns an asyncio.Task
+    that runs wait_random(max_delay).
+"""
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay):
