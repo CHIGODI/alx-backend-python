f39bac255f9539f5c0f6582d25f1ab7a597bfc94
CHIGODI <chiegody254@gmail.com>
2024-07-26 13:44:22 +0300

    Add task 2 soln

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,15 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Any) -> None:
         """Test access_nested_map with various inputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a'), KeyError),
+        ({"a": 1}, ('a', 'b'), KeyError)
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping,
+                                         path: Sequence,
+                                         expected_exception: Any) -> None:
+        """ """
+        with self.assertRaises(expected_exception):
+            access_nested_map(nested_map, path)
