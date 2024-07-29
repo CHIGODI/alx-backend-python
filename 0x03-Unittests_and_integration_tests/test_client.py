a399425eedc06519e37f940bad5e08adc760f2c2
CHIGODI <chiegody254@gmail.com>
2024-07-29 13:50:01 +0300

    Add soln for task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" """
+from parameterized import parameterized
+from client import GithubOrgClient
+import unittest
+from unittest.mock import patch, Mock
+from typing import Mapping
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ """
+    @parameterized.expand([
+        ('google', {}),
+        ('abc', {})
+    ])
+    @patch('utils.requests.get')
+    def test_org(self, org_name: str,
+                 expected: Mapping,
+                 mock_get) -> None:
+        """ """
+        org = GithubOrgClient(org_name)
+        mock_response = Mock()
+        mock_response.json.return_value = expected
+
+        mock_get.return_value = mock_response
+        result = org.org
+
+        self.assertEqual(result, expected)
+        mock_get.assert_called_once()
